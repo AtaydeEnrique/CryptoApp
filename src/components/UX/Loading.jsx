9d4696c9da9bd5c58aba2279dfba3b58f1a0193b
Atayde Enrique <riorat97@gmail.com>
2022-05-30 11:51:48 -0500

    change loader

--- src/components/UX/Loading.jsx
+++ src/components/UX/Loading.jsx
@@ -3,14 +3,10 @@ import React from 'react'
 import './Loading.css'
 const Loading = () => {
   return (
-    <div className='loader-container'>
-        <div className="loader">L</div>
-        <div className="loader">O</div>
-        <div className="loader">A</div>
-        <div className="loader">D</div>
-        <div className="loader">I</div>
-        <div className="loader">N</div>
-        <div className="loader">G</div>
+    <div className='loading-fullscreen'>
+      <div className='loader'>
+        <span>Loading</span>
+      </div>
     </div>
   )
 }
